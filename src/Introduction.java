c2e22372f08824975655587d7defb121dee7e66b
Juan Martin Borrini <juanmartinborrini758@gmail.com>
2023-04-16 00:26:53 -0300

    Initial commit. How to use comments and enter text

---
+++ src/Introduction.java
@@ -0,0 +1,7 @@
+public class Introduction {
+    public static void main(String[] args) {
+        // To comment a line
+        /* To comment multiple lines */
+        System.out.println("This is the form to write a simple text");
+    }
+}
