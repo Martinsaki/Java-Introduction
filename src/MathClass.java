a7799221f22bf4829637211940b70a4baf6e50d2
Juan Martin Borrini <juanmartinborrini758@gmail.com>
2023-04-16 21:59:57 -0300

    Added Math Class

---
+++ src/MathClass.java
@@ -0,0 +1,13 @@
+public class MathClass {
+    public static void main(String[] args) {
+        double x = 2.1;
+        double y = 5;
+        // Whit Math Class we use a series of element to program
+        // To access them you need to write "Math.element"
+        System.out.println(Math.random()); // random number
+        System.out.println(Math.ceil(x)); // round up
+        System.out.println(Math.floor(y)); // round down
+        System.out.println(Math.pow(x,y)); // x pow y
+        System.out.println(Math.sqrt(y)); // square
+    }
+}
